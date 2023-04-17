d9829975d569bb1800545b3874253105c77dd6f5
Mehmet Ali EROL <mehmetalierol@windowslive.com>
2023-04-17 12:12:54 +0300

    Logger settings added

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,8 +1,14 @@
+using Common;
 using Elastic.Apm.NetCoreAll;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+LoggingExtension.ConfigureLogging();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
